c80703e09b15f9cd86c56571bc2442adff0679ea
Pam Suebvisai <sinapam@proteus-tech.com>
2012-08-09 21:39:40 +0700

    added templatetags to package

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='datetime_tools',
-      version='1.3',
+      version='1.4',
       description='Datetime tools for Django',
       author='Proteus Technologies',
       author_email='team@proteus-tech.com',
       url='http://proteus-tech.com',
-      packages=['datetime_tools',],
+      packages=['datetime_tools', 'datetime_tools.templatetags'],
      )
